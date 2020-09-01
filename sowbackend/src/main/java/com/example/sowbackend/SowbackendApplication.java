a4132a8df0824fb46be26dded58af4b7e9cb9ead
Pallavi <sainalivela@gmail.com>
2020-09-01 12:55:32 -0400

    basic project stuctyre

---
+++ sowbackend/src/main/java/com/example/sowbackend/SowbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SowbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SowbackendApplication.class, args);
+	}
+
+}
---
+++ sowbackend/src/test/java/com/example/sowbackend/SowbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SowbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
